75e10b5c40409ccddd0160bc3bfd128fc1997a3e
jaysvishwagit <jaysvishwa@gmail.com>
2020-04-24 16:06:22 +0530

    final

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_moment import Moment
 
 app = Flask(__name__)
-moment = Moment(app)
+
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
